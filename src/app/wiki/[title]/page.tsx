ac38f5f7d89d87f6d3172a74b9eb769b2d686970
joldon <79482239+Joldon@users.noreply.github.com>
2024-07-16 17:50:37 +0200

    fixed a styling syntax error

--- src/app/wiki/[title]/page.tsx
+++ src/app/wiki/[title]/page.tsx
@@ -10,10 +10,9 @@ interface Params {
 export default async function WikiPage({ params }: Params) {
   const { title } = params;
   const content = await fetchPageContent(title);
-  console.log(content);
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1>{title.replace("_", " ")}</h1>
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
