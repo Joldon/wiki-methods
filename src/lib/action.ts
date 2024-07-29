ca2b1eee1dfb6c34da3de1469438f0193a6e5a66
joldon <79482239+Joldon@users.noreply.github.com>
2024-07-29 17:18:39 +0200

    server action for creating posts

---
+++ src/lib/action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import prisma from "./db";
+
+export async function createPost(formData: FormData) {
+  const post = await prisma.post.create({
+    data: {
+      title: formData.get("title") as string,
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
+      content: formData.get("content") as string,
+    },
+  });
+}
