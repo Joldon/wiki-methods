2119b0019efb8d988113b5ade44f69a79f6c6c49
joldon <79482239+Joldon@users.noreply.github.com>
2024-07-29 16:10:11 +0200

    posts page with a form

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,17 +2,29 @@ import prisma from "@/lib/db";
 import styles from "./posts.module.css";
 import Link from "next/link";
 const PostsPage = async () => {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+  });
   return (
-    <div>
-      <h1>All Posts (0)</h1>
-      <ul>
+    <div className={styles.container}>
+      <h1 className={styles.header}>All Posts ({posts.length})</h1>
+      <ul className={styles.ul}>
         {posts.map((post) => (
-          <li key={post.id}>
+          <li key={post.id} className={styles.li}>
             <Link href={`/posts/${post.slug}`}>{post.title}</Link>
           </li>
         ))}
       </ul>
+
+      {/* create a form with server action */}
+      <form method="post" action="/api/posts" className={styles.form}>
+        <h2 className={styles.subheader}>Create a new post</h2>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" required />
+        <label htmlFor="content">Content</label>
+        <textarea name="content" id="content" required></textarea>
+        <button type="submit">Create post</button>
+      </form>
     </div>
   );
 };
