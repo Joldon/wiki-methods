730ec92c092c02ca9f0df594c91a1181ee456199
joldon <79482239+Joldon@users.noreply.github.com>
2024-08-01 14:34:29 +0200

    CRUD post actions

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -2,7 +2,7 @@
 
 import prisma from "./db";
 
-export async function createPost(formData: FormData) {
+export const createPost = async (formData: FormData) => {
   const post = await prisma.post.create({
     data: {
       title: formData.get("title") as string,
@@ -12,4 +12,23 @@ export async function createPost(formData: FormData) {
       content: formData.get("content") as string,
     },
   });
-}
+};
+
+export const updatePost = async (formData: FormData, id: string) => {
+  const post = await prisma.post.update({
+    where: { id },
+    data: {
+      title: formData.get("title") as string,
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
+      content: formData.get("content") as string,
+    },
+  });
+};
+
+export const deletePost = async (id: string) => {
+  const post = await prisma.post.delete({
+    where: { id },
+  });
+};
