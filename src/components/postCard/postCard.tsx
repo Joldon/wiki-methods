4a4456d975d86acad94101bcda067b9d7e303b31
joldon <79482239+Joldon@users.noreply.github.com>
2024-09-13 13:07:42 +0200

    added type interface and content slice in postCard

--- src/components/postCard/postCard.tsx
+++ src/components/postCard/postCard.tsx
@@ -1,10 +1,17 @@
 import styles from "./postCard.module.css";
 
-const PostCard = ({ title, content }) => {
+interface PostCardProps {
+  title: string;
+  content: string;
+}
+
+const PostCard: React.FC<PostCardProps> = ({ title, content }) => {
   return (
     <div className={styles.postCard}>
       <h2 className={styles.postTitle}>{title}</h2>
-      <p className={styles.postContent}>{content}</p>
+      <p className={styles.postContent}>
+        {content && `${content.slice(0, 35)}...`}
+      </p>
     </div>
   );
 };
