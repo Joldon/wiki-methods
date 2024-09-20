144b2fd08d9c36427fe1b34ff55540f92a7e9c82
joldon <79482239+Joldon@users.noreply.github.com>
2024-09-20 15:33:40 +0200

    replacing index with more robust key and cleaning up Recommendation tool

--- src/components/recommendationTool/recommendationTool.tsx
+++ src/components/recommendationTool/recommendationTool.tsx
@@ -29,51 +29,7 @@ const RecommendationTool = () => {
             options={categories[category as keyof typeof categories]}
           />
         ))}
-        {/* <div>
-          <h3>Type</h3>
-          <label>
-            <input type="checkbox" name="quantitative" /> Quantitative
-          </label>
-          <label>
-            <input type="checkbox" name="qualitative" /> Qualitative
-          </label>
-        </div>
-
-        <div>
-          <h3>Reasoning</h3>
-          <label>
-            <input type="checkbox" name="deductive" /> Deductive
-          </label>
-          <label>
-            <input type="checkbox" name="inductive" /> Inductive
-          </label>
-        </div>
-
-        <div>
-          <h3>Level</h3>
-          <label>
-            <input type="checkbox" name="individual" /> Individual
-          </label>
-          <label>
-            <input type="checkbox" name="system" /> System
-          </label>
-          <label>
-            <input type="checkbox" name="global" /> Global
-          </label>
-        </div>
-
-        <div>
-          <h3>Time</h3>
-          <label>
-            <input type="checkbox" name="past" /> Past
-          </label>
-          <label>
-            <input type="checkbox" name="present" /> Present
-          </label>
-          <label>
-            <input type="checkbox" name="future" /> Future
-          </label>
-        </div> */}
+        <br />
 
         <button type="submit">Get Recommendations</button>
       </form>
@@ -82,8 +38,8 @@ const RecommendationTool = () => {
         <h2>Recommended Methods</h2>
         <ul>
           {methods.length > 0 ? (
-            methods.map((method, index) => (
-              <li key={index}>
+            methods.map((method) => (
+              <li key={method.method}>
                 <h3>{method.method}</h3>
                 <p>{method.description}</p>
               </li>
@@ -98,84 +54,3 @@ const RecommendationTool = () => {
 };
 
 export default RecommendationTool;
-
-// import styles from "./recommendationTool.module.css";
-// import { filterMethods, starterData } from "@/lib/starterData";
-
-// const RecommendationTool = async () => {
-//   const methods = await filterMethods(formData: FormData);
-//   return (
-//     <div className={styles.container}>
-//       <h1>Method Recommendation Tool</h1>
-
-//       <form action={filterMethods}>
-//         {/* Checkboxes for the criteria */}
-//         <div>
-//           <h3>Type</h3>
-//           <label>
-//             <input type="checkbox" name="quantitative" /> Quantitative
-//           </label>
-//           <label>
-//             <input type="checkbox" name="qualitative" /> Qualitative
-//           </label>
-//         </div>
-
-//         <div>
-//           <h3>Reasoning</h3>
-//           <label>
-//             <input type="checkbox" name="deductive" /> Deductive
-//           </label>
-//           <label>
-//             <input type="checkbox" name="inductive" /> Inductive
-//           </label>
-//         </div>
-
-//         <div>
-//           <h3>Level</h3>
-//           <label>
-//             <input type="checkbox" name="individual" /> Individual
-//           </label>
-//           <label>
-//             <input type="checkbox" name="system" /> System
-//           </label>
-//           <label>
-//             <input type="checkbox" name="global" /> Global
-//           </label>
-//         </div>
-
-//         <div>
-//           <h3>Time</h3>
-//           <label>
-//             <input type="checkbox" name="past" /> Past
-//           </label>
-//           <label>
-//             <input type="checkbox" name="present" /> Present
-//           </label>
-//           <label>
-//             <input type="checkbox" name="future" /> Future
-//           </label>
-//         </div>
-
-//         <button type="submit">Get Recommendations</button>
-//       </form>
-
-//       <div>
-//         <h2>Recommended Methods</h2>
-//         <ul>
-//           {methods.length > 0 ? (
-//             methods.map((method, index) => (
-//               <li key={index}>
-//                 <h3>{method.method}</h3>
-//                 <p>{method.description}</p>
-//               </li>
-//             ))
-//           ) : (
-//             <p>No methods match the selected criteria.</p>
-//           )}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RecommendationTool;
