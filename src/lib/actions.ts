8fad3365834767ebf28f67430766011b97d29ea5
joldon <joldon@gmail.com>
2024-11-28 11:58:42 +0100

    add error handling for duplicate post titles

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,19 +1,32 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import prisma from "./db";
 
 export const createPost = async (formData: FormData) => {
-  const post = await prisma.post.create({
-    data: {
-      title: formData.get("title") as string,
-      slug: (formData.get("title") as string)
-        .replace(/\s+/g, "-")
-        .toLowerCase(),
-      content: formData.get("content") as string,
-    },
-  });
-  revalidatePath("/posts");
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title: formData.get("title") as string,
+        slug: (formData.get("title") as string)
+          .replace(/\s+/g, "-")
+          .toLowerCase(),
+        content: formData.get("content") as string,
+      },
+    });
+    revalidatePath("/posts");
+    redirect("/posts");
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      redirect("/posts?error=duplicate-title");
+    }
+    throw error;
+  }
 };
 
 export const updatePost = async (formData: FormData, id: string) => {
