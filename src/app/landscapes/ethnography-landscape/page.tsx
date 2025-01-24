c2cbc1ea69d4cdf99ba8746d4d2f94f48d0c8966
joldon <joldon@gmail.com>
2025-01-24 14:32:48 +0100

    landscape page

---
+++ src/app/landscapes/ethnography-landscape/page.tsx
@@ -0,0 +1,5 @@
+const EthnographyPage = async () => {
+  return <div>EthnographyPage</div>;
+};
+
+export default EthnographyPage;
---
+++ src/app/landscapes/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandscapesPage = async () => {
+  return <div>LandscapesPage</div>;
+};
+
+export default LandscapesPage;
