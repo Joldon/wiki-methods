6f8d7a582d098488974906b106c57bc12da013bc
joldon <joldon@gmail.com>
2025-01-28 17:28:41 +0100

    created ethnography page and added forced-directed graph with json data

--- src/app/landscapes/ethnography-landscape/page.tsx
+++ src/app/landscapes/ethnography-landscape/page.tsx
@@ -1,5 +1,20 @@
+import ForceDirectedGraph from "@/components/charts/forceDirectedGraph";
+import mockData from "@/components/charts/mockData.json";
+import ethno from "@/components/charts/ethno.json";
+import ethno2 from "@/components/charts/ethno2.json";
+import miserables from "@/components/charts/miserables.json";
+import type { Node, Link } from "@/components/charts/forceDirectedGraph";
+
 const EthnographyPage = async () => {
-  return <div>EthnographyPage</div>;
+  const nodes = ethno2.nodes as Node[];
+  const links = ethno2.links as Link[];
+
+  return (
+    <div>
+      <h1>Force-Directed Graph</h1>
+      <ForceDirectedGraph nodes={nodes} links={links} />
+    </div>
+  );
 };
 
 export default EthnographyPage;
