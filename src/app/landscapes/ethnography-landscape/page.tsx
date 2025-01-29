0d99fab717977059e4be76df0a828ada5f01db17
joldon <joldon@gmail.com>
2025-01-29 19:43:31 +0100

    removed unused data json imports

--- src/app/landscapes/ethnography-landscape/page.tsx
+++ src/app/landscapes/ethnography-landscape/page.tsx
@@ -1,13 +1,12 @@
 import ForceDirectedGraph from "@/components/charts/forceDirectedGraph";
-import mockData from "@/components/charts/mockData.json";
+
 import ethno from "@/components/charts/ethno.json";
-import ethno2 from "@/components/charts/ethno2.json";
-import miserables from "@/components/charts/miserables.json";
+
 import type { Node, Link } from "@/components/charts/forceDirectedGraph";
 
 const EthnographyPage = async () => {
-  const nodes = ethno2.nodes as Node[];
-  const links = ethno2.links as Link[];
+  const nodes = ethno.nodes as Node[];
+  const links = ethno.links as Link[];
 
   return (
     <div>
