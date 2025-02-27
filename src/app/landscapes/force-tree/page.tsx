6eeb6094e017b0a8e871ed20d91448fcf9a73836
joldon <joldon@gmail.com>
2025-02-27 15:06:35 +0100

    clarified comment

--- src/app/landscapes/force-tree/page.tsx
+++ src/app/landscapes/force-tree/page.tsx
@@ -1,17 +1,14 @@
 import DirectedTree from "@/components/charts/directedTree";
 import tree from "@/components/charts/tree.json";
-
 import { TreeNode } from "@/components/charts/directedTree";
 
 const EthnographyTreePage = async () => {
-  // safe type for const data
-
+  // Properly cast the imported JSON to our TreeNode type
   const data = tree as TreeNode<{ name: string }>;
 
   return (
     <div style={{ width: "100%", height: "800px" }}>
       <h1>Force-Directed Tree</h1>
-
       <DirectedTree data={data} />
     </div>
   );
