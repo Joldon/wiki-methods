d3a96259b28fb1a9a81d319a6020a5d1045f0d02
joldon <joldon@gmail.com>
2026-01-08 15:00:12 +0100

    feat: include FeaturedContent component in HomePage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import FeaturedContent from "@/components/homepage/featuredContent";
 import Hero from "@/components/homepage/hero";
 import ValueProposition from "@/components/homepage/valueProposition";
+import FeaturedContentTest from "@/components/homepage/featuredContentTest";
 
 export default async function HomePage() {
   return (
     <main>
       <Hero />
       <ValueProposition />
+      <FeaturedContent />
+      {/* <FeaturedContentTest /> */}
     </main>
   );
 }
